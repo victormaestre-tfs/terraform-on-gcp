5f66d90502377a9bf5e96ed7d87e7b60ba1381b5
victormaestre-tfs <91181513+victormaestre-tfs@users.noreply.github.com>
2025-07-07 11:58:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "google_compute_instance" "pi-hole" {
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
+      size = 30
     }
   }
   network_interface {
