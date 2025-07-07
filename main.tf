da766bf0c0430f7861586b85ebef698e60ed2707
victormaestre-tfs <91181513+victormaestre-tfs@users.noreply.github.com>
2025-07-07 11:50:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-data "google_compute_image" "ubuntu" {
-  most_recent = true
-  project     = "ubuntu-os-cloud" 
-  family      = "ubuntu-minimal-2404-lts-amd64"
-}
-
 resource "google_compute_disk" "ubuntu" {
   name  = "pi-hole"
   size  = 30
@@ -12,10 +6,15 @@ resource "google_compute_disk" "ubuntu" {
   image = data.google_compute_image.ubuntu.self_link
 }
 
+data "google_compute_image" "ubuntu" {
+  most_recent = true
+  project     = "ubuntu-os-cloud" 
+  family      = "ubuntu-minimal-2404-lts-amd64"
+}
+
 resource "google_compute_instance" "pi-hole" {
   name         = "pi-hole"
   machine_type = "e2-micro"
-
   
   boot_disk {
     initialize_params {
