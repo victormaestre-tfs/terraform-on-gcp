e8ef6eca6cdc7bbe8a96a996cf2120bec168eacc
victormaestre-tfs <91181513+victormaestre-tfs@users.noreply.github.com>
2025-07-07 12:35:53 +0200

    Private IP

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "private_ip" {
+  value = google_compute_instance.pi-hole.network_interface[0].network_ip
+}
+
 output "public_ip" {
   value = google_compute_instance.pi-hole.network_interface[0].access_config.0.nat_ip
 }
